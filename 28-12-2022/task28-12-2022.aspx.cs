c9ac30ce31301927d6705f5e3a42f1f669589ad4
11-hayaalkhateeb <115453315+11-hayaalkhateeb@users.noreply.github.com>
2023-01-01 15:59:23 +0300

    Add files via upload

---
+++ 28-12-2022/task28-12-2022.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _28_12_2022
+{
+    public partial class task28_12_2022 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sql = new SqlConnection("data source=DESKTOP-6FI8FJC\\SQLEXPRESS;database=task28;integrated security=SSPI");
+            sql.Open();
+            string query = "insert into comment (comment) values ('" + textbox1 + "')";
+            SqlCommand com = new SqlCommand (sql, query);
+
+            Label1.Text =TextBox1.Text;
+            sql.Close;
+
+            
+
+
+        }
+    }
+}
