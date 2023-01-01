b12885f7807b64a5c18faec10231f521cf233cab
11-hayaalkhateeb <115453315+11-hayaalkhateeb@users.noreply.github.com>
2023-01-01 15:58:27 +0300

    Add files via upload

---
+++ task29-12-2022/list.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace task29_12_2022
+{
+    public partial class list : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox1.Text = DateTime.Now.ToString();
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+            TextBox1.Text = DateTime.Now.ToString();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HttpCookie k = new HttpCookie("color");
+            k.Values.Add("col", DropDownList1.SelectedValue);
+            k.Expires = DateTime.Now;
+            Response.Cookies.Add(k);
+            if (Request.Cookies["color"] != null)
+            {
+                if (Request.Cookies["color"]["col"] == "green")
+                {
+                    TextBox1.ForeColor = System.Drawing.Color.Green;
+                }
+                else if (Request.Cookies["color"]["col"] == "blue")
+                {
+                    TextBox1.ForeColor = System.Drawing.Color.Blue;
+                }
+                else if (Request.Cookies["color"]["col"] == "red")
+                {
+                    TextBox1.ForeColor = System.Drawing.Color.Red;
+                }
+                else
+                {
+                    TextBox1.ForeColor = System.Drawing.Color.Black;
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = DateTime.Now.ToString();
+        }
+    }
+    
+}
---
+++ task29-12-2022/task29.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace task29_12_2022
+{
+    public partial class task29 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Cookies["email"] != null && Request.Cookies["password"] != null)
+            {
+                email.Value = Request.Cookies["email"].Value;
+                password.Value = Request.Cookies["password"].Value;
+
+            }
+        }
+
+        protected void CheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CheckBox2.Checked)
+            {
+                Response.Cookies["email"].Value = email.Value;
+                Response.Cookies["password"].Value = password.Value;
+                Response.Cookies["email"].Expires = DateTime.Now.AddMinutes(1);
+                Response.Cookies["password"].Expires = DateTime.Now.AddMinutes(1);
+
+            }
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            email.Value = "";
+            password.Value = "";
+            Label1.Text = Request.Cookies["email"].Value + Request.Cookies["password"].Value;
+        }
+    }
+
+}
